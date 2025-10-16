1f601de5d3c8a98d55223194f47e3d77284237f0
Isac Zetterström <isac.zetterstrom@hotmail.com>
2025-10-17 01:12:43 +0200

    Added fetches to current pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { NavQuery } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+const nav = await client.fetch(NavQuery);
+
   return (
     <html lang="en">
       <body>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,10 @@
-function Projects() {
+import { ProjectsQuery } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+
+async function Projects() {
+
+    const projects = await client.fetch(ProjectsQuery);
+
     return (
         <div>Projects</div>
     )
